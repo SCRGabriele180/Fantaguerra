0bd3db53d5e077e29d885ac6df19695b2a2b2a35
SCRGabriele180 <111132844+SCRGabriele180@users.noreply.github.com>
2026-05-26 21:26:01 +0200

    Delete js directory

--- js/main.js
+++
@@ -1,9 +0,0 @@
-// Entry Point dell'applicazione
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Inizializza gli eventi della UI (Home, Bottoni, ecc.)
-    UI.init();
-    
-    // Mostra la schermata Home
-    UI.showScreen('home-screen');
-});
